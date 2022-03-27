025aecf542369826b02f7a12df00ee31a2f2e18a
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-27 13:44:57 -0400

    add buttons and center them in the middle of the screen by using flexbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex h-screen justify-center items-center">
+      <div className="space-x-[45px]">
+       <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded">SMALL FORM</button>
+       <button className="bg-purple-500 text-white font-bold py-2 px-4 rounded">LARGE FORM</button>
+      </div>
     </div>
   );
 }
